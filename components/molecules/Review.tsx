b362a3576965450340d7b165195a46526c77dbe2
Keita-0 <laughlaughkaeru@gmail.com>
2022-04-21 00:11:11 +0900

    リファクタリング

---
+++ components/molecules/Review.tsx
@@ -0,0 +1,85 @@
+import { memo } from "react";
+import styled from "styled-components";
+import { Star } from "../atoms/Star";
+
+type TypeReview = {
+  review: {
+    title: string;
+    id: string;
+    rate: number;
+    comment: string;
+    photo: string;
+  };
+};
+
+export const Review = memo((props: TypeReview) => {
+  const { review } = props;
+
+  return (
+    <SCard key={review.id}>
+      <div>
+        <SIMG
+          src={review.photo ? review.photo : "../../icon/20200506_noimage.png"}
+        />
+      </div>
+      <InfoArea>
+        <STitle key={review.id}>{review.title}</STitle>
+        <SDiv>
+          <Star rating={review.rate / 20} size={120} isNumber={true} />
+        </SDiv>
+        <SLabel>コメント</SLabel>
+        {review.comment === "" ||
+        review.comment === undefined ||
+        review.comment === null ? (
+          <Sp>コメントがありません。</Sp>
+        ) : (
+          <Sp>{review.comment}</Sp>
+        )}
+      </InfoArea>
+    </SCard>
+  );
+});
+
+const SCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 4px 10px rgb(0 0 0 / 5%), 0 0 1px rgb(0 0 0 / 10%);
+
+  width: 100%;
+  background-color: #eeeeee;
+  margin-bottom: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const InfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 3vh;
+`;
+
+const SIMG = styled.img`
+  height: 30vh;
+`;
+
+const STitle = styled.h2`
+  margin-top: 0;
+`;
+
+const SLabel = styled.p`
+  font-weight: bold;
+`;
+
+const Sp = styled.p`
+  margin: 0;
+`;
+
+const SDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+`;
---
+++ components/molecules/Search.tsx
@@ -0,0 +1,80 @@
+import { memo, useState } from "react";
+import styled from "styled-components";
+import toast from "react-hot-toast";
+import { useRouter } from "next/router";
+
+export const Search = memo(() => {
+  const [keyWord, setKeyWord] = useState("");
+  const router = useRouter();
+
+  const onChangeKeyWord = (e: any) => {
+    setKeyWord(e.target.value);
+  };
+
+  const checkKeyWord = (keyWord: string) => {
+    if (keyWord.trim() === "") {
+      toast.error("キーワードを入力してください");
+      return false;
+    }
+    return true;
+  };
+
+  const onClickSearch = () => {
+    if (checkKeyWord(keyWord)) {
+      router.push(`/home/search=${keyWord}&page=1`);
+    }
+    setKeyWord("");
+  };
+
+  const hoge = (e: any) => {
+    return e.preventDefault();
+  };
+
+  return (
+    <>
+      <SFrom method="GET" onSubmit={hoge}>
+        <InputText
+          type="text"
+          placeholder="映画を検索"
+          value={keyWord}
+          onChange={onChangeKeyWord}
+        />
+        <InputSubmit type="submit" onClick={onClickSearch}>
+          <Icon src="../../icon/検索アイコン1.png" />
+        </InputSubmit>
+      </SFrom>
+    </>
+  );
+});
+
+const SFrom = styled.form`
+  position: relative;
+  box-sizing: border-box;
+  border-bottom: 2px solid #999999;
+  display: flex;
+  align-items: center;
+  padding: 3px 10px;
+  border-radius: 3px;
+  height: 2.3em;
+  width: 20vw;
+  overflow: hidden;
+`;
+const InputText = styled.input`
+  border: none;
+  background: rgba(0, 0, 0, 0);
+  width: 100%;
+  height: 2em;
+  :focus {
+    outline: 0;
+  }
+`;
+const InputSubmit = styled.button`
+  cursor: pointer;
+  border: none;
+  margin-right: 0;
+  padding-right: 0;
+`;
+
+const Icon = styled.img`
+  height: 2vh;
+`;
