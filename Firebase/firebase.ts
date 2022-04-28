9ac4ba6864226492b6deb0cd12c7a5f064820810
Keita-0 <laughlaughkaeru@gmail.com>
2022-04-28 22:13:59 +0900

    リファクタリング

--- Firebase/firebase.ts
+++ Firebase/firebase.ts
@@ -1,25 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "@firebase/firestore";
+import { FirebaseApp, getApps, initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWHJzQwiNK3N_jHUZURUWHA9W9z0nKuZc",
-  authDomain: "movie-59b39.firebaseapp.com",
-  projectId: "movie-59b39",
-  storageBucket: "movie-59b39.appspot.com",
-  messagingSenderId: "24816755143",
-  appId: "1:24816755143:web:53966fe16972a9a0ec39e2",
-  measurementId: "G-Y48B49TKX2",
+  apiKey: "AIzaSyBZ5SUiwd98uQM7CbtOaP2Xl2E2VLh3efI",
+  authDomain: "movie-1eddc.firebaseapp.com",
+  projectId: "movie-1eddc",
+  storageBucket: "movie-1eddc.appspot.com",
+  messagingSenderId: "36602615591",
+  appId: "1:36602615591:web:077d7f5f5e9b149846bb02",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore();
+let app: FirebaseApp;
+let db: Firestore;
 
-export default { app, analytics };
+if (typeof window !== "undefined" && !getApps().length) {
+  app = initializeApp(firebaseConfig);
+  db = getFirestore();
+}
+
+export { db };
--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,16 +1,16 @@
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useRecoilState } from 'recoil';
-import { loginFlg } from '../../store/movieSearchState';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRecoilState } from "recoil";
+import { loginFlg } from "../store/movieSearchState";
 
 export const useAuth = () => {
   const auth = getAuth();
   const [isSignedIn, setIsSignedIn] = useRecoilState(loginFlg);
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      console.log('ログインしています。');
+      console.log("ログインしています。");
       setIsSignedIn(true);
     } else {
-      console.log('ログアウト中です。');
+      console.log("ログアウト中です。");
       setIsSignedIn(false);
     }
   });
